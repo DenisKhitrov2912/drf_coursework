b4acc9145795d4761a958efd4365db75994b615f
Khitrov <Hitrov.95@yandex.ru>
2024-06-05 13:38:03 +0300

    end of docker work

--- DRF_coursework/settings.py
+++ DRF_coursework/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
@@ -169,25 +169,23 @@ DATETIME_FORMAT = 'Y-m-dTH:i:sZ'
 TIME_FORMAT = 'H:i:s'
 
 CORS_ALLOWED_ORIGINS = [
-    'https://127.0.0.1:8000',  # Замените на адрес вашего фронтенд-сервера
+    'http://*', 'https://*',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    # Замените на адрес вашего фронтенд-сервера
-    "https://read-and-write.example.com",
-    # и добавьте адрес бэкенд-сервера
+    'http://*', 'https://*',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Настройки для Celery
 
 # URL-адрес брокера сообщений
-CELERY_BROKER_URL = 'redis://localhost:6379'  # Например, Redis,
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')  # Например, Redis,
 # который по умолчанию работает на порту 6379
 
 # URL-адрес брокера результатов, также Redis
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
 
 # Часовой пояс для работы Celery
 CELERY_TIMEZONE = 'Europe/Moscow'
